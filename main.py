d8cf851ce953e07631763d41cb296dc2b8d26fa0
benschaeffer17 <ben.schaeffer@gmail.com>
2026-03-14 20:10:28 -0400

    Tried to add the 600-cell and the 120-cell

--- main.py
+++ main.py
@@ -11,20 +11,26 @@ from widgets.ui import UserInterface, HeadsUpDisplay
 from widgets.capture import Capture
 from navigation.navigator import Navigator
 from navigation.rotator import Rotator
-from polytopes import get_24_cell, project_4d_to_3d
+from polytopes import get_24_cell, get_120_cell, get_600_cell, project_4d_to_3d
 from viz.style import Style, LineStyle
 import viz.drawing as drawing
 
 class App:
+    ZOOM_FACTOR = 1.2
+    MIN_ZOOM_FACTOR = 0.3
+    MAX_ZOOM_FACTOR = 10.0
+
     def __init__(self):
-        self.ui = UserInterface(title="24-Cell Visualizer")
+        self.ui = UserInterface(title="Polytope Visualizer")
         self.nav = Navigator(self.ui)
         self.hud = HeadsUpDisplay(self.ui.window)
         self.rotator = Rotator()
         self.capture = Capture(self.ui.window)
         self.ui.register_draw_function(self.draw)
         
-        self.vertices_4d, self.edges = get_24_cell()
+        self.shape_name = '24-cell'
+        self.load_shape()
+        
         self.angle_4d = 0.0
         
         self.style = Style()
@@ -33,7 +39,11 @@ class App:
         self.base_rotation_speed = 0.001
         self.last_frame_time = 0.0
 
+        self.default_camera_distance = 2.0
+        self.camera_distance = self.default_camera_distance
+
         self.ui.register_keyboard_callback(glfw.KEY_V, self.toggle_style)
+        self.ui.register_keyboard_callback(glfw.KEY_T, self.toggle_shape)
         self.ui.register_keyboard_callback(glfw.KEY_P, self.capture.toggle_recording)
         self.ui.register_keyboard_callback(glfw.KEY_1, lambda *args: self.set_rotation_plane(0))
         self.ui.register_keyboard_callback(glfw.KEY_2, lambda *args: self.set_rotation_plane(1))
@@ -43,95 +53,127 @@ class App:
         self.ui.register_keyboard_callback(glfw.KEY_6, lambda *args: self.set_rotation_plane(5))
         self.ui.register_keyboard_callback(glfw.KEY_A, self.increase_rotation_speed)
         self.ui.register_keyboard_callback(glfw.KEY_Z, self.decrease_rotation_speed)
+        self.ui.register_keyboard_callback(glfw.KEY_K, self.zoom_out)
+        self.ui.register_keyboard_callback(glfw.KEY_M, self.zoom_in)
 
+    def zoom_in(self, *args):
+        new_dist = self.camera_distance / self.ZOOM_FACTOR
+        min_dist = self.default_camera_distance * self.MIN_ZOOM_FACTOR
+        self.camera_distance = max(new_dist, min_dist)
 
-        # Vertex and Edge coloring based on 24-cell geometry
-        # The 24-cell vertices are the permutations of (±1, ±1, 0, 0).
-        # We can partition the vertices into three groups based on their w-coordinate
-        # in the initial projection, which corresponds to a cell-first projection of the 24-cell.
-        # w = -1: Inner octahedron (6 vertices) - rendered smaller
-        # w = 0:  Cuboctahedron (12 vertices)
-        # w = 1:  Outer octahedron (6 vertices) - rendered larger
+    def zoom_out(self, *args):
+        new_dist = self.camera_distance * self.ZOOM_FACTOR
+        max_dist = self.default_camera_distance * self.MAX_ZOOM_FACTOR
+        self.camera_distance = min(new_dist, max_dist)
 
-        v_inner_indices = {i for i, v in enumerate(self.vertices_4d) if v[3] == -1}
-        v_middle_indices = {i for i, v in enumerate(self.vertices_4d) if v[3] == 0}
-        v_outer_indices = {i for i, v in enumerate(self.vertices_4d) if v[3] == 1}
+    def load_shape(self):
+        if self.shape_name == '24-cell':
+            self.vertices_4d, self.edges = get_24_cell()
+        elif self.shape_name == '120-cell':
+            self.vertices_4d, self.edges = get_120_cell()
+        elif self.shape_name == '600-cell':
+            self.vertices_4d, self.edges = get_600_cell()
+        self.setup_coloring()
 
-        # --- Set Vertex Colors ---
-        # Assign colors based on the vertex groups.
-        cyan = [0.0, 1.0, 1.0]
-        orange = [1.0, 0.65, 0.0]
-        pale_red = [1.0, 0.5, 0.5]
-        self.colors = []
-        for i in range(len(self.vertices_4d)):
-            if i in v_inner_indices:
-                self.colors.append(cyan)
-            elif i in v_middle_indices:
-                self.colors.append(orange)
-            else: # outer
-                self.colors.append(pale_red)
-        self.colors = np.array(self.colors, dtype=np.float32)
+    def toggle_shape(self, *args):
+        if self.shape_name == '24-cell':
+            self.shape_name = '120-cell'
+        elif self.shape_name == '120-cell':
+            self.shape_name = '600-cell'
+        else:
+            self.shape_name = '24-cell'
+        self.load_shape()
 
-        # --- Set Edge Colors and Widths ---
-        # Edges are classified and colored based on the vertex groups they connect.
-        # A width multiplier is also assigned to each edge type for visual clarity.
-        self.edge_colors = []
-        self.edge_width_multipliers = []
-        
-        # Color definitions for edges
-        green = [0.0, 1.0, 0.0]  # Inner octahedron edges
-        red = [1.0, 0.0, 0.0]    # Inner to middle edges
-        blue = [0.0, 0.0, 1.0]   # Middle (cuboctahedron) edges
-        yellow = [1.0, 1.0, 0.0] # Middle to outer edges
-        purple = [1.0, 0.0, 1.0] # Outer octahedron edges
+    def setup_coloring(self):
+        if self.shape_name == '24-cell':
+            # Vertex and Edge coloring based on 24-cell geometry
+            v_inner_indices = {i for i, v in enumerate(self.vertices_4d) if v[3] == -1}
+            v_middle_indices = {i for i, v in enumerate(self.vertices_4d) if v[3] == 0}
+            v_outer_indices = {i for i, v in enumerate(self.vertices_4d) if v[3] == 1}
 
-        # Width multipliers, in descending order: Red > Green > Blue > Yellow > Purple
-        width_multipliers = {
-            "red": 1.2,
-            "green": 1.15,
-            "blue": 1.1,
-            "yellow": 1.05,
-            "purple": 1.0
-        }
+            # --- Set Vertex Colors ---
+            cyan = [0.0, 1.0, 1.0]
+            orange = [1.0, 0.65, 0.0]
+            pale_red = [1.0, 0.5, 0.5]
+            self.colors = []
+            for i in range(len(self.vertices_4d)):
+                if i in v_inner_indices:
+                    self.colors.append(cyan)
+                elif i in v_middle_indices:
+                    self.colors.append(orange)
+                else: # outer
+                    self.colors.append(pale_red)
+            self.colors = np.array(self.colors, dtype=np.float32)
 
-        for i, j in self.edges:
-            is_i_inner = i in v_inner_indices
-            is_i_middle = i in v_middle_indices
-            is_i_outer = i in v_outer_indices
+            # --- Set Edge Colors and Widths ---
+            self.edge_colors = []
+            self.edge_width_multipliers = []
+            
+            green = [0.0, 1.0, 0.0]
+            red = [1.0, 0.0, 0.0]
+            blue = [0.0, 0.0, 1.0]
+            yellow = [1.0, 1.0, 0.0]
+            purple = [1.0, 0.0, 1.0]
 
-            is_j_inner = j in v_inner_indices
-            is_j_middle = j in v_middle_indices
-            is_j_outer = j in v_outer_indices
+            width_multipliers = {
+                "red": 1.2, "green": 1.15, "blue": 1.1,
+                "yellow": 1.05, "purple": 1.0
+            }
 
-            if is_i_inner and is_j_inner:
-                self.edge_colors.append(green) # Central octahedron
-                self.edge_width_multipliers.append(width_multipliers["green"])
-            elif (is_i_inner and is_j_middle) or (is_i_middle and is_j_inner):
-                self.edge_colors.append(red) # Connecting inner to middle
-                self.edge_width_multipliers.append(width_multipliers["red"])
-            elif is_i_middle and is_j_middle:
-                self.edge_colors.append(blue) # Cuboctahedron
-                self.edge_width_multipliers.append(width_multipliers["blue"])
-            elif (is_i_middle and is_j_outer) or (is_i_outer and is_j_middle):
-                self.edge_colors.append(yellow) # Connecting middle to outer
-                self.edge_width_multipliers.append(width_multipliers["yellow"])
-            elif is_i_outer and is_j_outer:
-                self.edge_colors.append(purple) # Outer octahedron
-                self.edge_width_multipliers.append(width_multipliers["purple"])
-            else:
-                # Should not happen with the current 24-cell geometry
-                self.edge_colors.append([1.0, 1.0, 1.0])
-                self.edge_width_multipliers.append(1.0)
-        
-        self.edge_colors = np.array(self.edge_colors, dtype=np.float32)
-        self.edge_width_multipliers = np.array(self.edge_width_multipliers, dtype=np.float32)
+            for i, j in self.edges:
+                is_i_inner = i in v_inner_indices
+                is_i_middle = i in v_middle_indices
+                is_i_outer = i in v_outer_indices
+                is_j_inner = j in v_inner_indices
+                is_j_middle = j in v_middle_indices
+                is_j_outer = j in v_outer_indices
 
+                if is_i_inner and is_j_inner:
+                    self.edge_colors.append(green)
+                    self.edge_width_multipliers.append(width_multipliers["green"])
+                elif (is_i_inner and is_j_middle) or (is_i_middle and is_j_inner):
+                    self.edge_colors.append(red)
+                    self.edge_width_multipliers.append(width_multipliers["red"])
+                elif is_i_middle and is_j_middle:
+                    self.edge_colors.append(blue)
+                    self.edge_width_multipliers.append(width_multipliers["blue"])
+                elif (is_i_middle and is_j_outer) or (is_i_outer and is_j_middle):
+                    self.edge_colors.append(yellow)
+                    self.edge_width_multipliers.append(width_multipliers["yellow"])
+                elif is_i_outer and is_j_outer:
+                    self.edge_colors.append(purple)
+                    self.edge_width_multipliers.append(width_multipliers["purple"])
+                else:
+                    self.edge_colors.append([1.0, 1.0, 1.0])
+                    self.edge_width_multipliers.append(1.0)
+            
+            self.edge_colors = np.array(self.edge_colors, dtype=np.float32)
+            self.edge_width_multipliers = np.array(self.edge_width_multipliers, dtype=np.float32)
+        elif self.shape_name == '600-cell':
+            red = [1.0, 0.0, 0.0]
+            blue = [0.0, 0.0, 1.0]
+            green = [0.0, 1.0, 0.0]
+            self.colors = []
+            for v in self.vertices_4d:
+                if np.sum(np.abs(v)) == 1.0: # 8 vertices
+                    self.colors.append(red)
+                elif np.all(np.abs(v) == 0.5): # 16 vertices
+                    self.colors.append(blue)
+                else: # 96 vertices
+                    self.colors.append(green)
+            self.colors = np.array(self.colors, dtype=np.float32)
+            self.edge_colors = np.array([[1.0, 1.0, 1.0]] * len(self.edges), dtype=np.float32)
+            self.edge_width_multipliers = np.array([1.0] * len(self.edges), dtype=np.float32)
+        else: # 120-cell
+            self.colors = np.array([[1.0, 1.0, 1.0]] * len(self.vertices_4d), dtype=np.float32)
+            self.edge_colors = np.array([[1.0, 1.0, 1.0]] * len(self.edges), dtype=np.float32)
+            self.edge_width_multipliers = np.array([1.0] * len(self.edges), dtype=np.float32)
+    
     def set_rotation_plane(self, plane_index):
         self.rotation_plane = plane_index
 
     def increase_rotation_speed(self, *args):
         self.rotation_speed_level = min(10, self.rotation_speed_level + 1)
-
     def decrease_rotation_speed(self, *args):
         self.rotation_speed_level = max(0, self.rotation_speed_level - 1)
 
@@ -140,83 +182,53 @@ class App:
     
     def draw(self):
         current_time = glfw.get_time()
-        if self.last_frame_time == 0.0:
-            delta_time = 0.0
-        else:
-            delta_time = current_time - self.last_frame_time
-        
+        delta_time = (current_time - self.last_frame_time) if self.last_frame_time > 0 else 0.0
         if self.capture.recording:
             delta_time = 1.0 / 60.0
-
         self.last_frame_time = current_time
 
-        # Set projection matrix
         width, height = glfw.get_window_size(self.ui.window)
-        height = height if height > 0 else 1
-        aspect_ratio = width / height
-        
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
+        aspect_ratio = width / (height if height > 0 else 1)
+        
+        d = self.camera_distance
         if width >= height:
-            glOrtho(-2 * aspect_ratio, 2 * aspect_ratio, -2, 2, -10, 10)
+            glOrtho(-d * aspect_ratio, d * aspect_ratio, -d, d, -10, 10)
         else:
-            glOrtho(-2, 2, -2 / aspect_ratio, 2 / aspect_ratio, -10, 10)
+            glOrtho(-d, d, -d / aspect_ratio, d / aspect_ratio, -10, 10)
+        
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
-        # Setup lights in fixed space relative to the observer
         glLightfv(GL_LIGHT0, GL_POSITION, [0.0, 0.0, 7.0, 1.0])
         glLightfv(GL_LIGHT1, GL_POSITION, [5.0, 5.0, 0.0, 1.0])
         glLightfv(GL_LIGHT2, GL_POSITION, [-5.0, 5.0, 0.0, 1.0])
 
         glPushMatrix()
-        
-        # Apply 3D rotation from mouse
         self.nav.apply_rotation()
 
-        # Get 4D rotation matrix
         rotation_matrix = self.rotator.get_rotation_matrix(self.rotation_plane, self.angle_4d)
-
-        # Get fixed vertices
         plane_indices = self.rotator.planes[self.rotation_plane]
-        fixed_vertices_indices = set()
-        for i, v in enumerate(self.vertices_4d):
-            if abs(v[plane_indices[0]]) < 1e-6 and abs(v[plane_indices[1]]) < 1e-6:
-                fixed_vertices_indices.add(i)
+        fixed_vertices_indices = {i for i, v in enumerate(self.vertices_4d) if abs(v[plane_indices[0]]) < 1e-6 and abs(v[plane_indices[1]]) < 1e-6}
 
-        # Get projected 3D vertices
         projected_vertices = project_4d_to_3d(self.vertices_4d, rotation_matrix)
-        
-        # Draw the polytope
         drawing.draw(projected_vertices, self.edges, self.colors, self.style, fixed_vertices_indices=fixed_vertices_indices, edge_colors=self.edge_colors)
         
         glPopMatrix()
 
-        # Slowly rotate in 4D
         if self.rotation_speed_level > 0:
-            speed_factor = 1.5 ** (self.rotation_speed_level - 3)
-            self.angle_4d += self.base_rotation_speed * speed_factor * delta_time * 100.0
+            self.angle_4d += self.base_rotation_speed * (1.5 ** (self.rotation_speed_level - 3)) * delta_time * 100.0
 
         if self.capture.recording:
             self.capture.capture_frame()
 
-        # Draw the heads-up display
-        if self.style.line_style.style == LineStyle.LINE:
-            render_mode = "Wireframe"
-        else:
-            render_mode = "Cylinders"
-        
+        render_mode = "Wireframe" if self.style.line_style.style == LineStyle.LINE else "Cylinders"
         plane_name = self.rotator.get_plane_name(self.rotation_plane)
-        
-        if self.capture.recording:
-            capture_status = f"recording ({self.capture.frame_idx:04d})"
-        else:
-            capture_status = "stopped"
-
-        self.hud.draw(f"Render: {render_mode} | Rotation: {plane_name} | Speed: {self.rotation_speed_level} | FPS: {self.ui.fps} | Capture: {capture_status}")
+        capture_status = f"recording ({self.capture.frame_idx:04d})" if self.capture.recording else "stopped"
+        self.hud.draw(f"Shape: {self.shape_name} | Dist: {self.camera_distance:.2f} | Render: {render_mode} | Rotation: {plane_name} | Speed: {self.rotation_speed_level} | FPS: {self.ui.fps} | Capture: {capture_status}")
 
     def run(self):
-        # Initial OpenGL setup
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
@@ -224,7 +236,6 @@ class App:
         glEnable(GL_LIGHT2)
         glEnable(GL_COLOR_MATERIAL)
         glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
-        
         self.ui.run(self)
 
 if __name__ == '__main__':
--- polytopes.py
+++ polytopes.py
@@ -30,6 +30,108 @@ def get_24_cell():
             
     return vertices, edges
 
+def get_120_cell():
+    """
+    Returns the vertices and edges of a 120-cell.
+    """
+    phi = (1 + np.sqrt(5)) / 2
+    
+    vertices = set()
+
+    # Generate vertices
+    base_coords = [
+        (2, 2, 0, 0),
+        (np.sqrt(5), 1, 1, 1),
+        (phi, phi, phi, 1/phi**2),
+        (phi**2, 1/phi, 1/phi, 1/phi)
+    ]
+
+    for coords in base_coords:
+        for p in set(permutations(coords)):
+            for i in range(16): # signs
+                v = list(p)
+                if (i >> 0) & 1: v[0] *= -1
+                if (i >> 1) & 1: v[1] *= -1
+                if (i >> 2) & 1: v[2] *= -1
+                if (i >> 3) & 1: v[3] *= -1
+                vertices.add(tuple(v))
+    
+    vertices = np.array(list(vertices), dtype=np.float32)
+
+    # Generate edges
+    edges = []
+    edge_length_sq = (3 - np.sqrt(5))**2
+    for i, j in combinations(range(len(vertices)), 2):
+        v1 = vertices[i]
+        v2 = vertices[j]
+        dist_sq = np.sum((v1 - v2)**2)
+        if np.isclose(dist_sq, edge_length_sq, atol=1e-3):
+            edges.append((i, j))
+            
+    return vertices, edges
+
+def get_600_cell():
+    """
+    Returns the vertices and edges of a 600-cell.
+    The vertices are the 120 elements of the binary icosahedral group.
+    """
+    phi = (1 + np.sqrt(5)) / 2
+    vertices = set()
+
+    # 8 vertices of the form (±1, 0, 0, 0)
+    for p in set(permutations([1, 0, 0, 0])):
+        v = list(p)
+        vertices.add(tuple(v))
+        v[0] *= -1
+        vertices.add(tuple(v))
+
+    # 16 vertices of the form 1/2 * (±1, ±1, ±1, ±1)
+    for i in range(16):
+        v = [0.5, 0.5, 0.5, 0.5]
+        if (i >> 0) & 1: v[0] *= -1
+        if (i >> 1) & 1: v[1] *= -1
+        if (i >> 2) & 1: v[2] *= -1
+        if (i >> 3) & 1: v[3] *= -1
+        vertices.add(tuple(v))
+
+    # 96 vertices from even permutations of (0, ±1/2, ±1/(2φ), ±φ/2)
+    vals = [0, 0.5, 0.5 / phi, 0.5 * phi]
+    for p_vals in set(permutations(vals)):
+        p = list(p_vals)
+        # Check for even permutation
+        inversions = 0
+        for i in range(len(p)):
+            for j in range(i + 1, len(p)):
+                # Create a temporary list for comparison to handle the case where p[i] == p[j]
+                temp_p = list(p)
+                if temp_p[i] > temp_p[j]:
+                    inversions += 1
+        
+        if inversions % 2 == 0:
+            for i in range(8): # signs for non-zero elements
+                v = list(p)
+                k = 0
+                for j in range(4):
+                    if v[j] != 0:
+                        if (i >> k) & 1:
+                            v[j] *= -1
+                        k += 1
+                vertices.add(tuple(v))
+                
+    vertices = np.array(list(vertices), dtype=np.float32)
+    
+    # Edges
+    edges = []
+    edge_length_sq = (1/phi)**2
+    for i, j in combinations(range(len(vertices)), 2):
+        v1 = vertices[i]
+        v2 = vertices[j]
+        dist_sq = np.sum((v1 - v2)**2)
+        if np.isclose(dist_sq, edge_length_sq, atol=1e-3):
+            edges.append((i, j))
+
+    return vertices, edges
+
 def project_4d_to_3d(vertices, rotation_matrix=None):
     """
     Projects 4D vertices to 3D after applying a rotation.
